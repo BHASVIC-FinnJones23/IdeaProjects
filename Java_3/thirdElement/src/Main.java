e58cbf0bb6a902691ff773fdf17d4199ce015bcc
Finn <finn.jones23@students.bhasvic.ac.uk>
2024-01-30 15:43:16 +0000

    Start J3

---
+++ Java_3/thirdElement/src/Main.java
@@ -0,0 +1,11 @@
+// import the list so the program can use it
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        // create a list
+        ArrayList<String> list = new ArrayList<>();
+
+        // the list isn't used yet
+    }
+}
