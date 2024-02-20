8dee2395977fea23435a9e73e9f1df350797dd04
Finn <finn.jones23@students.bhasvic.ac.uk>
2024-02-20 15:43:37 +0000

    Classwork

--- Java_4/Statistics/src/Main.java
+++ Java_4/Statistics/src/Main.java
@@ -1,10 +1,63 @@
+import java.io.InputStream;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+
+
+
         Statistics statistics = new Statistics();
+        /*
         statistics.addNumber(3);
         statistics.addNumber(5);
         statistics.addNumber(1);
         statistics.addNumber(2);
         System.out.println("Count: " + statistics.getCount());
+        System.out.println("Sum: " + statistics.sum());
+        System.out.println("Average: " + statistics.average());
+
+         */
+        Scanner scanner = new Scanner(System.in);
+
+        /*
+
+        //Next bit for sum of user input 1
+        System.out.println("Enter Numbers (-1 to end):");
+        int currentInput = 0;
+        while(true){
+            currentInput = Integer.parseInt(scanner.nextLine());
+            if(currentInput==-1){
+                break;
+            }else{
+                statistics.addNumber(currentInput);
+            }
+        }
+        System.out.println("Count: " + statistics.getCount());
+        System.out.println("Sum: " + statistics.sum());
+        System.out.println("Average: " + statistics.average());
+
+         */
+
+        Statistics statisticsOdd = new Statistics();
+        Statistics statisticsEven = new Statistics();
+        int lastValue = 0;
+
+        while(true){
+            lastValue = Integer.parseInt(scanner.nextLine());
+            if(lastValue==-1){
+                break;
+            }
+            statistics.addNumber(lastValue);
+            if(lastValue%2!=0){
+                //odd
+                statisticsOdd.addNumber(lastValue);
+            }else{
+                //even
+                statisticsEven.addNumber(lastValue);
+            }
+        }
+        System.out.println("🔥: " + statistics.sum());
+        System.out.println("Sum of even numbers: " + statisticsEven.sum());
+        System.out.println("Sum of odd numbers: " + statisticsOdd.sum());
+
     }
 }
--- Java_4/Statistics/src/statistics.java
+++ Java_4/Statistics/src/statistics.java
@@ -1,16 +1,42 @@
 import java.util.ArrayList;
  class Statistics {
     private int count;
+     private int sum;
+     private double average;
     private ArrayList<Integer> values = new ArrayList<>();
 
 
     public Statistics(){
         this.count = 0;
+        this.average=0;
+        this.sum = 0;
     }
     public void addNumber(int number){
-        values.add(number);
+        this.values.add(number);
         this.count +=1;
     }
+     public int sum() {
+         // write code here
+         this.sum = 0;
+         for(int i = 0;i<this.count;i++){
+             this.sum += this.values.get(i);
+         }
+         return this.sum;
+     }
+
+     public double average() {
+         // write code here
+         this.sum = 0;
+         for(int i = 0;i<this.count;i++){
+             this.sum += this.values.get(i);
+         }
+         if(this.count != 0) {
+             this.average = (double)this.sum/(double)this.count;
+         }else{
+             this.average=0;
+         }
+         return this.average;
+     }
     public int getCount(){
         return this.count;
     }
